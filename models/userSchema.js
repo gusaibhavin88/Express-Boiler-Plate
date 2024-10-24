cc88633cabd57e8468e91ee5c69d6141debc27a9
Bhavin <gusaibhavin88@gmail.com>
2024-10-24 12:34:19 +0530

    changes up

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,19 +1,7 @@
 const mongoose = require("mongoose");
 const { mongo_connection } = require("../config/connection");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    profile_image: { type: String },
-    contact_number: { type: Number },
-    birth_date: { type: Date },
-    reset_password_token: { type: String },
-    is_deleted: { type: Boolean, default: false },
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({}, { timestamps: true });
 
 const User = mongo_connection.model("user", userSchema);
 module.exports = User;
